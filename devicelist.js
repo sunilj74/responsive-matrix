25c258d8dcfb63edbde8e0503b2c6fe77a2d5362
Sunil Joseph <sunilj74@gmail.com>
2019-09-03 13:01:19 -0400

    Added device list

---
+++ devicelist.js
@@ -0,0 +1,335 @@
+[  
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone XR",
+      "pixelWidth": 828,
+      "pixelHeight": 1792,
+      "width": 414,
+      "height": 896
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone XS",
+      "pixelWidth": 1125,
+      "pixelHeight": 2436,
+      "width": 375,
+      "height": 812
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone XS Max",
+      "pixelWidth": 1242,
+      "pixelHeight": 2688,
+      "width": 414,
+      "height": 896
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone X",
+      "pixelWidth": 1125,
+      "pixelHeight": 2436,
+      "width": 375,
+      "height": 812
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone 8 Plus",
+      "pixelWidth": 1080,
+      "pixelHeight": 1920,
+      "width": 414,
+      "height": 736
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone 8",
+      "pixelWidth": 750,
+      "pixelHeight": 1334,
+      "width": 375,
+      "height": 667
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone 7 Plus",
+      "pixelWidth": 1080,
+      "pixelHeight": 1920,
+      "width": 414,
+      "height": 736
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone 7",
+      "pixelWidth": 750,
+      "pixelHeight": 1334,
+      "width": 375,
+      "height": 667
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone 6 Plus/6S Plus",
+      "pixelWidth": 1080,
+      "pixelHeight": 1920,
+      "width": 414,
+      "height": 736
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone 6/6S",
+      "pixelWidth": 750,
+      "pixelHeight": 1334,
+      "width": 375,
+      "height": 667
+   },
+   {  
+      "os": "ios",
+      "type": "phone",
+      "name": "iPhone 5",
+      "pixelWidth": 640,
+      "pixelHeight": 1136,
+      "width": 320,
+      "height": 568
+   },
+   {  
+      "os": "ios",
+      "type": "tablet",
+      "name": "iPad Pro",
+      "pixelWidth": 2048,
+      "pixelHeight": 2732,
+      "width": 1024,
+      "height": 1366
+   },
+   {  
+      "os": "ios",
+      "type": "tablet",
+      "name": "iPad Third & Fourth Generation",
+      "pixelWidth": 1536,
+      "pixelHeight": 2048,
+      "width": 768,
+      "height": 1024
+   },
+   {  
+      "os": "ios",
+      "type": "tablet",
+      "name": "iPad Air 1 & 2",
+      "pixelWidth": 1536,
+      "pixelHeight": 2048,
+      "width": 768,
+      "height": 1024
+   },
+   {  
+      "os": "ios",
+      "type": "tablet",
+      "name": "iPad Mini 2 & 3",
+      "pixelWidth": 1536,
+      "pixelHeight": 2048,
+      "width": 768,
+      "height": 1024
+   },
+   {  
+      "os": "ios",
+      "type": "tablet",
+      "name": "iPad Mini",
+      "pixelWidth": 768,
+      "pixelHeight": 1024,
+      "width": 768,
+      "height": 1024
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Nexus 6P",
+      "pixelWidth": 1440,
+      "pixelHeight": 2560,
+      "width": 412,
+      "height": 732
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Nexus 5X",
+      "pixelWidth": 1080,
+      "pixelHeight": 1920,
+      "width": 412,
+      "height": 732
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Google Pixel 3 XL",
+      "pixelWidth": 1440,
+      "pixelHeight": 2960,
+      "width": 412,
+      "height": 847
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Google Pixel 3",
+      "pixelWidth": 1080,
+      "pixelHeight": 2160,
+      "width": 412,
+      "height": 824
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Google Pixel 2 XL",
+      "pixelWidth": 1440,
+      "pixelHeight": 2560,
+      "width": 412,
+      "height": 732
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Google Pixel XL",
+      "pixelWidth": 1440,
+      "pixelHeight": 2560,
+      "width": 412,
+      "height": 732
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Google Pixel",
+      "pixelWidth": 1080,
+      "pixelHeight": 1920,
+      "width": 412,
+      "height": 732
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Samsung Galaxy Note 9",
+      "pixelWidth": 1440,
+      "pixelHeight": 2960,
+      "width": 360,
+      "height": 740
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Samsung Galaxy Note 5",
+      "pixelWidth": 1440,
+      "pixelHeight": 2560,
+      "width": 480,
+      "height": 853
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "LG G5",
+      "pixelWidth": 1440,
+      "pixelHeight": 2560,
+      "width": 480,
+      "height": 853
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "One Plus 3",
+      "pixelWidth": 1080,
+      "pixelHeight": 1920,
+      "width": 480,
+      "height": 853
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Samsung Galaxy S9+",
+      "pixelWidth": 1440,
+      "pixelHeight": 2960,
+      "width": 360,
+      "height": 740
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Samsung Galaxy S9",
+      "pixelWidth": 1440,
+      "pixelHeight": 2960,
+      "width": 360,
+      "height": 740
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Samsung Galaxy S8+",
+      "pixelWidth": 1440,
+      "pixelHeight": 2960,
+      "width": 360,
+      "height": 740
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Samsung Galaxy S8",
+      "pixelWidth": 1440,
+      "pixelHeight": 2960,
+      "width": 360,
+      "height": 740
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Samsung Galaxy S7 Edge",
+      "pixelWidth": 1440,
+      "pixelHeight": 2560,
+      "width": 360,
+      "height": 640
+   },
+   {  
+      "os": "android",
+      "type": "phone",
+      "name": "Samsung Galaxy S7",
+      "pixelWidth": 1440,
+      "pixelHeight": 2560,
+      "width": 360,
+      "height": 640
+   },
+   {  
+      "os": "android",
+      "type": "tablet",
+      "name": "Nexus 9",
+      "pixelWidth": 1536,
+      "pixelHeight": 2048,
+      "width": 768,
+      "height": 1024
+   },
+   {  
+      "os": "android",
+      "type": "tablet",
+      "name": "Nexus 7 (2013)",
+      "pixelWidth": 1200,
+      "pixelHeight": 1920,
+      "width": 600,
+      "height": 960
+   },
+   {  
+      "os": "android",
+      "type": "tablet",
+      "name": "Samsung Galaxy Tab 10",
+      "pixelWidth": 800,
+      "pixelHeight": 1280,
+      "width": 800,
+      "height": 1280
+   },
+   {  
+      "os": "android",
+      "type": "tablet",
+      "name": "Chromebook Pixel",
+      "pixelWidth": 2560,
+      "pixelHeight": 1700,
+      "width": 1280,
+      "height": 850
+   }
+]
